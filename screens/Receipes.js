d8979761133f3664c15c477dc6a56ed144c3cb85
maruhardik04 <49114615+maruhhardik04@users.noreply.github.com>
2022-11-26 11:35:35 +0530

    Update Receipes.js

--- screens/Receipes.js
+++ screens/Receipes.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect,useContext } from 'react'
-import {SafeAreaView, StyleSheet, View, Text, Image,TouchableOpacity} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text, Image,TouchableOpacity,TextInput} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -7,20 +7,23 @@ const Receipes = ({navigation,route}) => {
 
   const [isLoading, setLoading] = useState(true);
   const [receipes,setReceipes] = useState([]);
-
+  const [filterData,setFilterData]= useState([]);
+  const [search,setSearch]=useState('');
   
   const getReceipes = async () => {
     try {
      const response = await fetch('http://10.1.50.13:8000/item/');
      console.log(response);
      const json = await response.json();
-     console.log(route);
+   
      if(route.params?.items == undefined)
      {
        setReceipes(json);
+       setFilterData(json);
      }
      else{
       setReceipes(route.params?.items);
+      setFilterData(route.params?.items);
      }
    } catch (error) {
      console.error(error);
@@ -29,7 +32,29 @@ const Receipes = ({navigation,route}) => {
    }
  }
 
- 
+ const searchFilter = (text) => {
+  if(text)
+  {
+    const newData= receipes.filter((item)=>{
+
+      const itemData = item.title 
+                      ?item.title.toUpperCase()
+                      : ''.toUpperCase();
+                      
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;                 
+    });
+    setFilterData(newData);
+    setSearch(text);
+  }
+  else
+  {
+      setFilterData(receipes);
+      setSearch(text);
+  }
+}
+
+
  useEffect(()=>{
   getReceipes();
 
@@ -64,10 +89,29 @@ const Receipes = ({navigation,route}) => {
   };
   return (
     <SafeAreaView style={{backgroundColor: '#fff', flex: 1,paddingTop:50,paddingBottom:20}}>
+    
+    <View
+        style={{
+          marginTop: 40,
+          flexDirection: 'row',
+          paddingHorizontal: 20,
+        }}>
+        <View style={style.inputContainer}>
+          <Icon name="search" size={28} />
+          <TextInput
+            style={{flex: 1, fontSize: 18}}
+            placeholder="Search for food"
+            value={search}
+            onChangeText={(text) => searchFilter(text)}
+          />
+        </View>
+      
+      </View>
+    
       <FlatList
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{paddingBottom: 80}}
-        data={receipes}
+        data={filterData}
         renderItem={({item}) => <CartCard item={item} />}
       />
     </SafeAreaView>
@@ -101,6 +145,16 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     alignContent: 'center',
   },
+  inputContainer: {
+    flex: 1,
+    height: 50,
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: '#E5E5E5',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  }
 });
 
 export default Receipes;
+
